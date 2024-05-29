0646ba06617d2b54f42f28228047c5792649442c
Nguyen Duy <nguyenduyshopf@gmail.com>
2024-05-30 00:13:46 +0700

    Update post api

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,11 +1,33 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    title: 'First Post',
-    content: 'This is the first post!',
+    posts: [
+      {
+        _id: new Date().getTime().toString(),
+        title: 'First Post',
+        content: 'This is the first post!',
+        imageUrl: 'images/duck.jpg',
+        creator: {
+          name: 'Duy Nguyen',
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({
+        message:
+          'Validation failed, entered data is incorrect',
+        errors: errors.array(),
+      });
+  }
   const title = req.body.title;
   const content = req.body.content;
 
@@ -13,9 +35,11 @@ exports.createPost = (req, res, next) => {
   res.status(201).json({
     message: 'Post create successfully!',
     post: {
-      id: new Date().toISOString(),
+      _id: new Date().getTime().toString(),
       title: title,
       content: content,
+      creator: { name: 'Duy Nguyen' },
+      createdAt: new Date(),
     },
   });
 };
