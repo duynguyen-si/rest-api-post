34cb00bcb5e38163400a93b5405cd971e042e16f
Nguyen Duy <nguyenduyshopf@gmail.com>
2024-06-06 01:29:04 +0700

    Storedata to db

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -22,4 +23,11 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(
+    'mongodb+srv://NguyenDuy:Voicoibebong1@cluster0.tulklbo.mongodb.net/messages'
+  )
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
