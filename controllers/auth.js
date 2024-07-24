e822b67ffed3443490e7d988a51dc70485894ead
Nguyen Duy <nguyenduyshopf@gmail.com>
2024-07-25 02:24:37 +0700

    update authen

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const {
 } = require('express-validator');
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
@@ -31,6 +32,51 @@ exports.signup = (req, res, next) => {
       res.status(201).json({
         message: 'User created',
         userId: result._id,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error(
+          'No user found with this email.'
+        );
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then((isEqual) => {
+      if (!isEqual) {
+        const error = new Error('Wrong password!');
+        error.statusCode = 401;
+        throw error;
+      }
+
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString(),
+        },
+        'secretkey',
+        { expiresIn: '1h' }
+      );
+
+      res.status(200).json({
+        token: token,
+        userId: loadedUser._id.toString(),
       });
     })
     .catch((err) => {
